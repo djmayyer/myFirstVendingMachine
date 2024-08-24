188020c4cf2dd1e7d54ed7817ebd6e89de937c54
DJmayyer <horia.zamfir@yahoo.com>
2024-08-24 13:45:38 +0300

    Initial Commit

---
+++ myFirstVendingMachine/myFirstVendingMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstVendingMachineApp.swift
+//  myFirstVendingMachine
+//
+//  Created by Horia Zamfir on 24.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct myFirstVendingMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myFirstVendingMachineTests/myFirstVendingMachineTests.swift
@@ -0,0 +1,16 @@
+//
+//  myFirstVendingMachineTests.swift
+//  myFirstVendingMachineTests
+//
+//  Created by Horia Zamfir on 24.08.2024.
+//
+
+import Testing
+
+struct myFirstVendingMachineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
